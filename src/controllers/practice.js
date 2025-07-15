5fb4cbe3fc7825a1f08c414d53d5805aa748f504
aryabenade <benadearya@gmail.com>
2025-07-16 00:06:10 +0530

    fixed bugs in refreshAccessToken

--- src/controllers/practice.js
+++
@@ -1,8 +0,0 @@
-const a={
-    name:"arya",
-    age:21,
-    b:2
-}
-
-const {b,name}=a
-console.log(b,name);
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -226,16 +226,16 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
             httpOnly: true,
             secure: true
         }
-        const { accessToken, newRefreshToken } = await generateAccessAndRefreshTokens(user._id)
+        const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(user._id)
     
         return res
         .status(200)
         .cookie("accessToken",accessToken,options)
-        .cookie("refreshToken",newRefreshToken,options)
+        .cookie("refreshToken",refreshToken,options)
         .json(
             new ApiRespose(
                 200,
-                {accessToken,refreshToken:newRefreshToken},
+                {accessToken,refreshToken},
                 "Access Token refreshed"
             )
         )
